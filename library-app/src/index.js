aa10a8a0a6f4daea97150525e1d6cf86c83bb96a
duynk190077 <duypro10x123@gmail.com>
2022-11-23 16:03:54 +0700

    app-feature/route: add routing, redux, theme to app

--- library-app/src/index.js
+++ library-app/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import { store } from './redux/store';
+// import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './components/theme/theme'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={store}> */}
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
 
---
+++ library-app/src/routes/constant.js
@@ -0,0 +1,15 @@
+import { path } from "../constant/path";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+
+
+export const PageRoutes = [
+    {
+        path: path.HOME,
+        element: Home
+    },
+    {
+        path: path.LOGIN,
+        element: Login
+    }
+]
