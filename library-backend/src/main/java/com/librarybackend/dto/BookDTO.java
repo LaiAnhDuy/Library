dbea8e93a30849300e5eb5e4ab8d118036530a13
dqd051020 <duywap@gmail.com>
2023-01-06 22:23:23 +0700

    set up book service

---
+++ library-backend/src/main/java/com/librarybackend/dto/BookDTO.java
@@ -0,0 +1,43 @@
+package com.librarybackend.dto;
+
+import com.librarybackend.entity.BookEntity;
+import lombok.*;
+
+import javax.persistence.Column;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDTO extends BaseDTO{
+    private Long categoryId;
+    private String title;
+    private String description;
+    private int totalPages;
+    private String author;
+    private String publisher;
+    private String language;
+    private int price;
+    private LocalDateTime boughtDate;
+
+    public BookDTO(BookEntity bookEntity) {
+        this.categoryId = bookEntity.getCategoryId();
+        this.title = bookEntity.getTitle();
+        this.description = bookEntity.getDescription();
+        this.totalPages = bookEntity.getTotalPages();
+        this.author = bookEntity.getAuthor();
+        this.publisher = bookEntity.getPublisher();
+        this.language = bookEntity.getLanguage();
+        this.price = bookEntity.getPrice();
+        this.boughtDate = bookEntity.getBoughtDate();
+
+        this.id = bookEntity.getId();
+        this.code = bookEntity.getCode();
+        this.createdBy = bookEntity.getCreatedBy();
+        this.createdDate = bookEntity.getCreatedDate();
+        this.modifiedBy = bookEntity.getModifiedBy();
+        this.modifiedDate = bookEntity.getModifiedDate();
+        this.deleted = bookEntity.isDeleted();
+    }
+}
---
+++ library-backend/src/main/java/com/librarybackend/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.librarybackend.repository;
+
+import com.librarybackend.entity.BookEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends BaseRepository<BookEntity> {
+
+    Page<BookEntity> findByCategoryIdAndDeletedFalse(Long id, Pageable pageable);
+}
