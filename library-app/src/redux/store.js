7df5a3077906a05da78b09f6525dfb1e00e7bc01
Laianhduy <laianhduy04042001@gmail.com>
2023-01-31 16:12:59 +0700

    app-feature/admin: add redux slice

--- library-app/src/redux/store.js
+++ library-app/src/redux/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import alertSlice from './alertSlice';
+import bookSlice from './bookSlice';
+import loadingSlice from './loadingSlice';
 import orderSlice from './orderSlice';
+import reloadSlice from './reloadSlice';
 
 export const store = configureStore({
   reducer: {
-    order: orderSlice
+    loading: loadingSlice,
+    alert: alertSlice,
+    order: orderSlice,
+    book: bookSlice,
+    reload: reloadSlice
   },
 });
