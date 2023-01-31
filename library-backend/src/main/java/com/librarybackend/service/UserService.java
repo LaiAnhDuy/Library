2516e513c0f8b43f559684f8fd51757e2ce17c33
Duong <nguyenduong985911@gmail.com>
2023-01-31 10:20:33 +0700

    fix bug encrypt password

--- library-backend/src/main/java/com/librarybackend/service/UserService.java
+++ library-backend/src/main/java/com/librarybackend/service/UserService.java
@@ -3,17 +3,25 @@ package com.librarybackend.service;
 import com.librarybackend.dto.UserDTO;
 import com.librarybackend.entity.UserEntity;
 import com.librarybackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class UserService extends BaseService<UserRepository, UserEntity> {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public UserDTO createUser(UserDTO userToCreate) {
         UserEntity entityToCreate = new UserEntity(userToCreate);
+        entityToCreate.setPassword(passwordEncoder.encode(entityToCreate.getPassword()));
+        entityToCreate.setCreatedDate(LocalDateTime.now());
         UserEntity createdUser = save(entityToCreate);
         return new UserDTO(createdUser);
     }
